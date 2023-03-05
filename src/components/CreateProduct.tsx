222c735c1d2cb62ef73f536f06257619771b5fda
RomaSamkov <Romansamkov.rs@gmail.com>
2023-03-05 20:50:45 +0200

    Create CreateProduct.tsx

---
+++ src/components/CreateProduct.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function CreateProduct() {
+  return (
+    <form action="">
+      <input
+        type="text"
+        className="border py-2 px-4 mb-2 w-full outline-none"
+        placeholder="Enter product title"
+      />
+      <button type="submit" className="py-2 px-4 border bg-yellow-400">
+        Create
+      </button>
+    </form>
+  );
+}
