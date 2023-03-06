98c18a6638ee457a75f9588eb23e99d64b948f50
RomaSamkov <Romansamkov.rs@gmail.com>
2023-03-06 21:42:39 +0200

    Update products.ts

--- src/hooks/products.ts
+++ src/hooks/products.ts
@@ -8,6 +8,10 @@ export function useProducts() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
+  function addProduct(product: IProduct) {
+    setProducts((prev) => [...prev, product]);
+  }
+
   async function fethProducts() {
     try {
       setError("");
@@ -28,5 +32,5 @@ export function useProducts() {
     fethProducts();
   }, []);
 
-  return { products, loading, error };
+  return { products, loading, error, addProduct };
 }
