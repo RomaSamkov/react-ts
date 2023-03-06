d2b3f12b53b89ed7657e2673c13f10f0119c4bc7
RomaSamkov <Romansamkov.rs@gmail.com>
2023-03-06 21:43:00 +0200

    Create ProductsPage.tsx

---
+++ src/pages/ProductsPage.tsx
@@ -0,0 +1,40 @@
+import React, { useContext } from "react";
+import CreateProduct from "../components/CreateProduct";
+import ErrorMessage from "../components/ErrorMessage";
+import Loader from "../components/Loader";
+import Modal from "../components/Modal";
+import { Product } from "../components/Product";
+import { ModalContext } from "../context/ModalContext";
+import { useProducts } from "../hooks/products";
+import { IProduct } from "../models";
+
+export default function ProductsPage() {
+  const { products, loading, error, addProduct } = useProducts();
+  const { modal, open, close } = useContext(ModalContext);
+
+  const createHandler = (product: IProduct) => {
+    close();
+    addProduct(product);
+  };
+
+  return (
+    <div className="container mx-auto max-w-2xl pt-5">
+      {loading && <Loader />}
+      {error && <ErrorMessage error={error} />}
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+      {modal && (
+        <Modal title="Create New Product" onClose={close}>
+          <CreateProduct onCreate={createHandler} />
+        </Modal>
+      )}
+      <button
+        onClick={open}
+        className="fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2 hover:bg-yellow-400"
+      >
+        +
+      </button>
+    </div>
+  );
+}
