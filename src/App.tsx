813004da5b14b5cb33fa69f41f2184e72196ca38
RomaSamkov <Romansamkov.rs@gmail.com>
2023-03-06 21:42:28 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
-// import CreateProduct from "./components/CreateProduct";
-import ErrorMessage from "./components/ErrorMessage";
-import Loader from "./components/Loader";
-// import Modal from "./components/Modal";
-import { Product } from "./components/Product";
-import { useProducts } from "./hooks/products";
+import { Routes, Route } from "react-router-dom";
+import AboutPage from "./pages/AboutPage";
+import ProductsPage from "./pages/ProductsPage";
+import Navigation from "./components/Navigation";
 
 function App() {
-  const { products, loading, error } = useProducts();
   return (
-    <div className="container mx-auto max-w-2xl pt-5">
-      {loading && <Loader />}
-      {error && <ErrorMessage error={error} />}
-      {products.map((product) => (
-        <Product key={product.id} product={product} />
-      ))}
-      {/* <Modal title="Create New Product">
-        <CreateProduct />
-      </Modal> */}
-    </div>
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<ProductsPage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </>
   );
 }
 
