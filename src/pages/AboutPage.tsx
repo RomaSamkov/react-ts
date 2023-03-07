e76599a437b141fb92243f5b181b6c3a4ea43df1
RomaSamkov <Romansamkov.rs@gmail.com>
2023-03-07 19:20:26 +0200

    Update AboutPage.tsx

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <div className="flex flex-col items-center text-white">
+      <h2 className="text-2xl mt-6 mb-6">This is my draft while studying:</h2>
+      <ul className="text-yellow-400 text-lg">
+        <li className="mb-2"> - React + TypeScript</li>
+        <li className="mb-2"> - Tailwind CSS</li>
+        <li className="mb-2"> - react-router-dom</li>{" "}
+        <li className="mb-2"> - HTTP requests in React + TypeScript</li>
+        <li> - used Fake Store API</li>
+      </ul>
+    </div>
+  );
 }
